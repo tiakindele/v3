61feda26fb0f1988f6b26f6d5bef25e1c14deb0d
Tolu Akindele <tolu@akindele.ca>
2021-03-27 17:42:22 -0700

    Change footer icons

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,8 +1,8 @@
 import React from "react"
 import PropTypes from 'prop-types';
-import IconGithub from "./Github"
-import IconLinkedIn from "./LinkedIn"
-import IconMail from "./Mail"
+import IconGithub from "./github"
+import IconLinkedIn from "./linkedin"
+import IconMail from "./mail"
 
 const Icon = ({ name, color }) => {
   switch (name.toLowerCase()) {
