80aaed24e1dc169e75576292a597601f89189806
Tolu Akindele <tolu@akindele.ca>
2021-04-09 12:19:06 -0700

    Set target="_blank" rel="noopener noreferrer" on external links

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import styled, { ThemeProvider } from "styled-components"
 import PropTypes from "prop-types"
 
@@ -29,7 +29,36 @@ const Layout = ({ children, location }) => {
   const [theme, themeToggler, mountedComponent] = useDarkMode();
   const themeMode = theme === 'light' ? lightTheme : darkTheme;
 
-  if(!mountedComponent) return <div/>
+  // Sets target="_blank" rel="noopener noreferrer" on external links
+  const handleExternalLinks = () => {
+    const allLinks = Array.from(document.querySelectorAll('a'));
+    if (allLinks.length > 0) {
+      allLinks.forEach(link => {
+        if (link.host !== window.location.host) {
+          link.setAttribute('rel', 'noopener noreferrer');
+          link.setAttribute('target', '_blank');
+        }
+      });
+    }
+  };
+
+  useEffect(() => {
+    if (location.hash) {
+      // const id = location.hash.substring(1); // location.hash without the '#'
+      // setTimeout(() => {
+      //   const el = document.getElementById(id);
+      //   if (el) {
+      //     el.scrollIntoView();
+      //     el.focus();
+      //   }
+      // }, 0);
+    }
+
+    handleExternalLinks();
+  }, []);
+
+  // if(!mountedComponent) return <div/>
+
   return (
     <ThemeProvider theme={themeMode}>
       <>
